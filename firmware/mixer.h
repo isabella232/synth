ece8d98e8e8ff6ff85bf90decb8b9e65533499a8
Gabriel Taubman <gtaubman@gmail.com>
2018-12-27 14:10:30 -0500

    Add a 2-channel mixer with saturating arithmetic.

---
+++ firmware/mixer.h
@@ -0,0 +1,16 @@
+#ifndef __MIXER_H__
+#define __MIXER_H__
+
+#include <stdint.h>
+
+typedef struct Mixer {
+	uint16_t gain_a, gain_b;
+} Mixer;
+
+// Sets the gain for a two channel mixer from 0 to 1.
+void MixerSetGain(Mixer* mixer, float a, float b);
+
+// Returns the mixed value of the two inputs.
+uint16_t MixerMix(Mixer* mixer, uint16_t a, uint16_t b);
+
+#endif  // __MIXER_H__
